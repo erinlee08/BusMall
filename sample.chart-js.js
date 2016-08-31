bb72e2a81fb469a248e9dc5dbef0ade5e1aa88b7
Erin <erinylee@yahoo.com>
2016-08-31 00:40:03 -0700

    practice with canvas

---
+++ sample.chart-js.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('practice');
+var ctx = canvas.createElement('2d');
+console.log('hola',ctx);
+ctx.fillRect(0,100,100,250);
+cxt.moveTo(150,150);
+cxt.lineTo(300,300);
